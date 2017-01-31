8c52197a0d81b5ea4a75aaababe4eec68a988db1
Nishtha Rathore <Nishtha Rathore@MyPersonal>
2017-01-31 18:34:03 +0530

    ThirdCommit

---
+++ src/main/resources/WebConfig.java
@@ -0,0 +1,13 @@
+package restfrontend1.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "backend1")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
